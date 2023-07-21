6ceaea2820d4c2a30c054e607604998496e2f036
carlalap <carlos3_alarcon@hotmail.com>
2023-07-21 10:37:50 -0500

    11. Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,7 +9,7 @@ from models.user import User
 from flask import jsonify, abort, request
 
 
-# Retrieves the list of all Place objects of a City: 
+# Retrieves the list of all Place objects of a City:
 # GET /api/v1/cities/<city_id>/places
 @app_views.route("/cities/<city_id>/places", methods=["GET"],
                  strict_slashes=False)
@@ -21,6 +21,7 @@ def get_places_by_city(city_id):
     places = city.places
     return jsonify([place.to_dict() for place in places])
 
+
 # Retrieves a Place object. : GET /api/v1/places/<place_id>
 @app_views.route("/places/<place_id>", methods=["GET"], strict_slashes=False)
 def get_place(place_id):
@@ -29,6 +30,7 @@ def get_place(place_id):
     if not place:
         abort(404)
     return jsonify(place.to_dict())
+
 
 # Deletes a Place object: DELETE /api/v1/places/<place_id>
 @app_views.route("/places/<place_id>", methods=["DELETE"],
@@ -41,6 +43,7 @@ def delete_place(place_id):
     storage.save()
     return jsonify({}), 200
 
+
 # Creates a Place: POST /api/v1/cities/<city_id>/places
 @app_views.route("/cities/<city_id>/places", methods=["POST"],
                  strict_slashes=False)
@@ -49,24 +52,25 @@ def create_place(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    
+
     place_data = request.get_json()
     if not place_data:
         abort(400, "Not a JSON")
 
     if "user_id" not in place_data.keys():
         abort(400, "Missing user_id")
-    
+
     user = storage.get(User, place_data.get("user_id"))
     if not user:
         abort(404)
-    
+
     if "name" not in place_data.keys():
         abort(400, "Missing name")
-    
+
     place_data["city_id"] = city_id
     place = Place(**place_data)
     return jsonify(place.to_dict()), 201
+
 
 # Updates a Place object: PUT /api/v1/places/<place_id>
 @app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
