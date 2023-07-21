08eacbd91684976dec6de79b44fdbe006f3dab5c
carlalap <carlos3_alarcon@hotmail.com>
2023-07-20 23:16:23 -0500

    update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Script that starts an API! """
-
+""" Script that starts an API! """
 from flask import Flask, jsonify
-import os
 from models import storage
 from api.v1.views import app_views
+import os
 
 
-"""Flask start"""
+""" Flask start """
 app = Flask(__name__)
 
-"""Register the blueprint app_views
-for Flask instance"""
+""" Register the blueprint app_views
+for Flask instance """
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Method to handle that calls storage.close()"""
+    """ Method to handle that calls storage.close() """
     storage.close()
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True)
+    API_HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    API_PORT = int(os.getenv('HBNB_API_PORT', '5000'))
+
+    app.run(host=API_HOST, port=API_PORT, threaded=True)
