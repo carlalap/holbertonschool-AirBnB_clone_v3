5ba1adb145fb68cc260e77688e8c7d17cd60519a
carlalap <carlos3_alarcon@hotmail.com>
2023-07-21 17:56:07 -0500

    11. Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,33 +8,33 @@ from models.user import User
 from flask import jsonify, abort, request
 
 
-@app_views.route("/cities/<city_id>/places",
-                 methods=["GET"], strict_slashes=False)
-def retrieves_all_places(city_id):
-    """Returns the list of all Place objects"""
+# Retrieves the list of all Place objects of a City:
+# GET /api/v1/cities/<city_id>/places
+@app_views.route("/cities/<city_id>/places", methods=["GET"],
+                 strict_slashes=False)
+def get_places_by_city(city_id):
+    """Retrieves the list of all place objects by city"""
     city = storage.get(City, city_id)
     if not city:
         abort(404)
     places = city.places
-    places_list = []
-    for place in places:
-        places_list.append(place.to_dict())
-    return jsonify(places_list)
+    return jsonify([place.to_dict() for place in places])
 
 
+# Retrieves a Place object. : GET /api/v1/places/<place_id>
 @app_views.route("/places/<place_id>", methods=["GET"], strict_slashes=False)
 def get_place(place_id):
-    """Returns an object by id"""
+    """Retrieves a Place object."""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
     return jsonify(place.to_dict())
 
 
+# Deletes a Place object: DELETE /api/v1/places/<place_id>
 @app_views.route("/places/<place_id>", methods=["DELETE"],
                  strict_slashes=False)
 def delete_place(place_id):
-    """Deletes an object by id"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -43,6 +43,7 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
+# Creates a Place: POST /api/v1/cities/<city_id>/places
 @app_views.route("/cities/<city_id>/places",
                  methods=["POST"], strict_slashes=False)
 def create_place(city_id):
@@ -70,6 +71,7 @@ def create_place(city_id):
     return jsonify(place.to_dict()), 201
 
 
+# Updates a Place object: PUT /api/v1/places/<place_id>
 @app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
 def update_place(place_id):
     """Updates an object"""
