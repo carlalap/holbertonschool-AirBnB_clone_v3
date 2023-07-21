e24a1bfc96873aa871585ace04ac148c6f97be1d
carlalap <carlos3_alarcon@hotmail.com>
2023-07-20 22:18:29 -0500

    update 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,9 @@ from models import storage, classes
 
 
 # created for Task_4. Status of your API
+# create a route /status on the object app_views
+# that returns a JSON: "status": "OK"
+# curl -X GET http://0.0.0.0:5000/api/v1/status
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Endpoint to return the status as JSON"""
@@ -15,12 +18,12 @@ def get_status():
 
 
 # create for task 5
-@app_views.route('/api/v1/stats', methods=['GET'])
-def get_stats():
-    """Retrieve the number of each objects by type"""
-    stats = {}
-    for cls_name, cls in classes.items():
-        count = storage.count(cls)
-        stats[cls_name] = count
-
-    return jsonify(stats)
+# @app_views.route('/api/v1/stats', methods=['GET'])
+# def get_stats():
+#    """Retrieve the number of each objects by type"""
+#    stats = {}
+#    for cls_name, cls in classes.items():
+#        count = storage.count(cls)
+#        stats[cls_name] = count
+#
+#    return jsonify(stats)
