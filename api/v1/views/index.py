3a19bb320f957b5340891d6bd9f59001041ceb61
carlalap <carlalap>
2023-07-22 11:53:31 -0700

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,13 +6,6 @@ from flask import jsonify
 from models import storage, classes
 
 
-
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    """Route to return a JSON response with status OK"""
-    return jsonify(status="OK")
-
-
 @app_views.route('/api/v1/stats', methods=['GET'])
 def get_stats():
     """Retrieve the number of each objects by type"""
