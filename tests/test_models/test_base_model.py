89fd61c480df7730c86f20a74068a279b49bfe5d
carlalap <carlos3_alarcon@hotmail.com>
2023-07-21 22:26:22 -0500

    update test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -82,24 +82,21 @@ class TestBaseModel(unittest.TestCase):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
         value."""
-        tic = datetime.now()
+
         inst1 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-
-        # Introduce a larger time delay (e.g., 1 second)
-        # to ensure unique timestamps
-        time.sleep(1)
-
-        tic = datetime.now()
         inst2 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
 
+        self.assertEqual(inst1.created_at, inst1.created_at)
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
+
+    def test_datetime_attributes2(self):
+        """Test that two BaseModel instances
+        have different datetime objects"""
+        inst1 = BaseModel()
+        inst2 = BaseModel()
+
         self.assertNotEqual(inst1.created_at, inst2.created_at)
-        self.assertNotEqual(inst1.updated_at, inst2.updated_at)
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
