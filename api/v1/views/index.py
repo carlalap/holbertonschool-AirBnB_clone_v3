6004e928ae89d41916082b911fe4ffa05f09d6a1
carlalap <carlos3_alarcon@hotmail.com>
2023-07-21 19:45:07 -0500

    update index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,9 +20,15 @@ def status():
 @app_views.route('/api/v1/stats', methods=['GET'])
 def get_stats():
     """Retrieve the number of each objects by type"""
-    stats = {}
-    for cls_name, cls in classes.items():
-        count = storage.count(cls)
-        stats[cls_name] = count
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    json_dict = json.dumps(dic, indent=2)
+    return json_dict
+    
 
-    return jsonify(stats)
