f394c5b523fcb954522143cdd9a2c87a5fad539d
carlalap <carlalap>
2023-07-22 18:48:51 -0700

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,9 +4,13 @@ Flask Class and import modules"""
 
 from flask import Blueprint
 
-# Create a Blueprint instance with the url prefix /api/v1
+# create a variable app_views which is an instance of 
+# Blueprint (url prefix must be /api/v1)
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
-# Wildcard import of everything in the package api.v1.views.index
+# wildcard import of everything in the package 
+# api.v1.views.index => PEP8 will complain about it, 
+# don’t worry, it’s normal and this file 
+# (v1/views/__init__.py) won’t be check.
 from api.v1.views.index import *
